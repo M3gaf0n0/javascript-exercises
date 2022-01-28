77759123f5f46f8bf8c828868eeb70ea05a056f8
M3gaf0n0 <gigan.sinretorno@gmail.com>
2022-01-28 13:30:41 -0600

    Exercise 8 incomplete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	var suma = num1 + num2;
+	return suma;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	var subs = num1 - num2;
+	return subs;
 };
 
-const sum = function() {
-	
+const sum = function(num1, num2) {
+	var sums = num1;
+	for (let i=1; i<arguments.length; i++) {
+		sums = sums + arguments[i];
+	}
+	return sums;
+	console.log(sums);
 };
 
-const multiply = function() {
-
+const multiply = function(num1, num2) {
+	var mul = 1;
+	for (let i=0; i<arguments.length; i++) {
+		mul *= arguments[i];
+	}
+	return mul;
+	console.log(mul);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	var pow = num1;
+	for (let i=1; i<num2; i++) {
+		pow = pow * num1;
+	}
+	return pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	var fact = 1;
+	for (let i=1; i<=num; i++) {
+		fact = fact * i;
+	}
+	return fact;
 };
 
 // Do not edit below this line
